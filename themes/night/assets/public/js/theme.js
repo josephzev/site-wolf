dea96c3e4191e791e8c39df2be207363caa2d99f
josephzev <yjotkowitz@gmail.com>
2021-06-06 10:33:23 +0700

    changes

--- themes/night/assets/public/js/theme.js
+++ themes/night/assets/public/js/theme.js
@@ -50,14 +50,14 @@ UIkit.util.on('#sec3', 'inview', function () {
 UIkit.util.on('#section3', 'scrolled', function () {
   document.getElementById("body").style.overflowY = "visible";
 });
+/*toggle-nav*/
+
 UIkit.util.on('#secfinal', 'inview', function () {
   UIkit.scroll('#sectionfinal').scrollTo('#sectionfinal');
   document.getElementById("nav1").style.backgroundColor = "#110011";
   document.getElementById("nav2").style.backgroundColor = "#110011";
   document.getElementById("svg").style.display = "block";
 });
-/*toggle-nav*/
-
 UIkit.util.on('#secfinal', 'outview', function () {
   document.getElementById("nav1").style.backgroundColor = "transparent";
   document.getElementById("nav2").style.backgroundColor = "transparent";
@@ -70,6 +70,29 @@ UIkit.util.on('#section2', 'scrolled', function () {
     repeat: true
   });
   document.getElementById("body").style.overflowY = "visible";
+});
+/*services*/
+
+UIkit.util.on('#sec-faq', 'scrolled', function () {
+  UIkit.scrollspy('#sec-service', {
+    repeat: true
+  });
+});
+UIkit.util.on('#sec-service', 'inview', function () {
+  UIkit.scroll('#section-service').scrollTo('#section-service');
+});
+UIkit.util.on('#sec-faq', 'inview', function () {
+  UIkit.scroll('#sec-faq').scrollTo('#sec-faq');
+  document.getElementById("nav1").style.backgroundColor = "#000";
+  document.getElementById("nav2").style.backgroundColor = "#000";
+  document.getElementById("nav1").style.boxShadow = "black 0px 5px 15px";
+  document.getElementById("nav2").style.boxShadow = "black 0px 5px 15px;";
+});
+UIkit.util.on('#sec-faq', 'outview', function () {
+  document.getElementById("nav1").style.backgroundColor = "transparent";
+  document.getElementById("nav2").style.backgroundColor = "transparent";
+  document.getElementById("nav1").style.boxShadow = "none";
+  document.getElementById("nav2").style.boxShadow = "none";
 });
 
 /***/ }),
--- themes/night/assets/resources/js/theme.js
+++ themes/night/assets/resources/js/theme.js
@@ -39,6 +39,10 @@ UIkit.util.on('#section3', 'scrolled', function () {
 
 });
 
+
+/*toggle-nav*/
+
+
 UIkit.util.on('#secfinal', 'inview', function () {
 	    
 	    UIkit.scroll('#sectionfinal').scrollTo('#sectionfinal');
@@ -53,8 +57,6 @@ UIkit.util.on('#secfinal', 'inview', function () {
 	    
 });
 
-/*toggle-nav*/
-
 UIkit.util.on('#secfinal', 'outview', function () {
 
 	    
@@ -63,6 +65,9 @@ UIkit.util.on('#secfinal', 'outview', function () {
 
 	    document.getElementById("svg").style.display = "none";
 });
+
+
+
 
 /*first sec*/
 
@@ -75,5 +80,49 @@ UIkit.util.on('#section2', 'scrolled', function () {
 	    	document.getElementById("body").style.overflowY = "visible";
 
 });
+
+
+
+/*services*/
+
+UIkit.util.on('#sec-faq', 'scrolled', function () {   
+
+
+          UIkit.scrollspy('#sec-service', {
+	    		repeat:true
+	    	});  
+
+});
+
+
+UIkit.util.on('#sec-service', 'inview', function () {
+
+	    
+	    UIkit.scroll('#section-service').scrollTo('#section-service');
+
+
+});
+
+UIkit.util.on('#sec-faq', 'inview', function () {
+
+	    
+	    UIkit.scroll('#sec-faq').scrollTo('#sec-faq');
+
+	    document.getElementById("nav1").style.backgroundColor = "#000";
+	    document.getElementById("nav2").style.backgroundColor = "#000";
+	    document.getElementById("nav1").style.boxShadow = "black 0px 5px 15px";
+	    document.getElementById("nav2").style.boxShadow = "black 0px 5px 15px;";
+
+});
+
+UIkit.util.on('#sec-faq', 'outview', function () {
+
+	    
+	    document.getElementById("nav1").style.backgroundColor = "transparent";
+	    document.getElementById("nav2").style.backgroundColor = "transparent";
+	    document.getElementById("nav1").style.boxShadow = "none";
+	    document.getElementById("nav2").style.boxShadow = "none";
+});
+
 
 
